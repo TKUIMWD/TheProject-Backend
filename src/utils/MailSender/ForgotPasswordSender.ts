aaa9114309b3f5d12b1919e45420156f9d50459b
powen <powen0966091716@gmail.com>
2025-05-02 11:45:41 +0800

    更新 驗證信URL為前端頁面
    next : 由前端驗證頁面發送實際驗證請求

--- src/utils/MailSender/ForgotPasswordSender.ts
+++ src/utils/MailSender/ForgotPasswordSender.ts
@@ -5,8 +5,9 @@ const BASE_URL = process.env.BASE_URL;
 
 export function sendForgotPasswordEmail(toMail: string, token: string) {
     const subject = '重置密碼';
+    const frontendUrl = process.env.FRONTEND_BASE_URL+`/forgotPassword?token=${token}`;
     const text = `請點擊下面的連結重置密碼以繼續使用我們的服務：
-${BASE_URL}/api/v1/auth/forgotPassword/?token=${token}\n如果您沒有申請重置密碼，請忽略此郵件。`
+${frontendUrl}\n如果您沒有申請重置密碼，請忽略此郵件。`
            
     transporter.sendMail({
         ...mailConfigurations(toMail, subject, text)
@@ -18,7 +19,3 @@ ${BASE_URL}/api/v1/auth/forgotPassword/?token=${token}\n如果您沒有申請重
         logger.error(`Failed to send password reset email to ${toMail}`);
     });
 }
-
-/*
-之後連結改成前端頁面，而不是直接連到後端，在前端填寫新密碼後透過API回傳給後端
-*/
