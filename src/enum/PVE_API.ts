29fc5566d0bba24c18cb6612af71e1a97b133e53
powen <powen0966091716@gmail.com>
2025-07-12 09:55:51 +0800

    add several PVE_APIs

--- src/enum/PVE_API.ts
+++ src/enum/PVE_API.ts
@@ -4,4 +4,7 @@ const pve_api_base = process.env.PVE_API_BASE_URL;
 
 export const pve_api: PVEApiEndPoints = {
     access_ticket: `${pve_api_base}/access/ticket`,  // /access/ticket
+    nodes: `${pve_api_base}/nodes`, // /nodes
+    nodes_qemu: (node: string) => `${pve_api_base}/nodes/${node}/qemu`, // /nodes/{node}/qemu
+    qemu_config: (node: string, vmid: string) => `${pve_api_base}/nodes/${node}/qemu/${vmid}/config`, // /nodes/{node}/qemu/{vmid}/config
 };
--- src/interfaces/ApiEndPoints.ts
+++ src/interfaces/ApiEndPoints.ts
@@ -1,3 +1,6 @@
 export interface PVEApiEndPoints {
     access_ticket: string;  // /access/ticket
+    nodes: string; // /nodes
+    qemu_config: (node: string, vmid: string) => string; // /nodes/{node}/qemu/{vmid}/config
+    nodes_qemu: (node: string) => string; // /nodes/{node}/qemu
 }
