09996489e62a93e8275b601a94eb66adf375a18c
powen <powen0966091716@gmail.com>
2025-07-12 10:09:35 +0800

    Update PVE_API.ts

--- src/enum/PVE_API.ts
+++ src/enum/PVE_API.ts
@@ -3,8 +3,15 @@ import { PVEApiEndPoints } from "../interfaces/ApiEndPoints";
 const pve_api_base = process.env.PVE_API_BASE_URL;
 
 export const pve_api: PVEApiEndPoints = {
-    access_ticket: `${pve_api_base}/access/ticket`,  // /access/ticket
-    nodes: `${pve_api_base}/nodes`, // /nodes
-    nodes_qemu: (node: string) => `${pve_api_base}/nodes/${node}/qemu`, // /nodes/{node}/qemu
-    qemu_config: (node: string, vmid: string) => `${pve_api_base}/nodes/${node}/qemu/${vmid}/config`, // /nodes/{node}/qemu/{vmid}/config
+    access_ticket: `${pve_api_base}/access/ticket`,  // POST /access/ticket
+    nodes: `${pve_api_base}/nodes`, // GET /nodes
+    nodes_qemu: (node: string) => `${pve_api_base}/nodes/${node}/qemu`, // POST /nodes/{node}/qemu
+    qemu_config: (node: string, vmid: string) => `${pve_api_base}/nodes/${node}/qemu/${vmid}/config`, // GET /nodes/{node}/qemu/{vmid}/config
 };
+
+/*
+access_ticket: 取得訪問 ticket
+nodes: 取得節點列表
+nodes_qemu: 創建 QEMU 虛擬機
+qemu_config: 取得 QEMU 虛擬機配置
+*/
