0632a935083b55db32b4428c299bb43fa4e60462
hung-0621 <kenny0909362798@gmail.com>
2025-09-21 13:09:11 +0800

    修正：User 無法訪問 Box template

--- src/service/VMManageService.ts
+++ src/service/VMManageService.ts
@@ -1445,10 +1445,10 @@ export class VMManageService extends Service {
             const { template_info, qemuConfig } = templateResult.body;
 
             // 檢查範本是否公開或用戶為 owner
-            if (!template_info.is_public && (!template_info.owner || template_info.owner !== user._id.toString())) {
-                logger.warn(`User ${user.username} (${user._id}) is not allowed to use template ${box.vmtemplate_id}`);
-                return createResponse(403, "You do not have permission to use this template");
-            }
+            // if (!template_info.is_public && (!template_info.owner || template_info.owner !== user._id.toString())) {
+            //     logger.warn(`User ${user.username} (${user._id}) is not allowed to use template ${box.vmtemplate_id}`);
+            //     return createResponse(403, "You do not have permission to use this template");
+            // }
 
             // 檢查資源限制
             const resourceCheckResult = await this._checkResourceLimits(user, cpuCores, memorySize, diskSize);
