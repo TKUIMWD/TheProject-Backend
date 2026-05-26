4ac3ffea6c7b6613af38180bcae5b4d0ba2c1785
powen <powen0966091716@gmail.com>
2026-05-26 21:09:21 +0800

    refactor super admin service request context forwarding

--- src/service/SuperAdminService.ts
+++ src/service/SuperAdminService.ts
@@ -6,44 +6,43 @@ import { superAdminRequestAdapterService } from "../modules/super-admin/SuperAdm
 import { validateTokenAndGetSuperAdminUser } from "../utils/auth";
 import { createResponse, resp } from "../utils/resp";
 
+type SuperAdminServiceInput = {
+    actor: User;
+    body: Request["body"];
+};
+
 export class SuperAdminService extends Service {
     public async changeUserRole(request: Request): Promise<resp<undefined>> {
-        return this.withSuperAdmin(request, "changeUserRole", "Error changing user role", (actor) =>
-            superAdminRequestAdapterService.changeUserRole({
-                actor,
-                body: request.body
-            })
+        return this.withSuperAdminInput(request, "changeUserRole", "Error changing user role", (input) =>
+            superAdminRequestAdapterService.changeUserRole(input)
         );
     }
 
     public async assignCRPToUser(request: Request): Promise<resp<any | undefined>> {
-        return this.withSuperAdmin(request, "assignCRPToUser", "Error assigning CRP to user", (actor) =>
-            superAdminRequestAdapterService.assignCRPToUser({
-                actor,
-                body: request.body
-            })
+        return this.withSuperAdminInput(request, "assignCRPToUser", "Error assigning CRP to user", (input) =>
+            superAdminRequestAdapterService.assignCRPToUser(input)
         );
     }
 
     public async getAllUsers(request: Request): Promise<resp<User[] | undefined>> {
-        return this.withSuperAdmin(request, "getAllUsers", "Error getting all users", (actor) =>
-            superAdminRequestAdapterService.getAllUsers(actor),
+        return this.withSuperAdminInput(request, "getAllUsers", "Error getting all users", (input) =>
+            superAdminRequestAdapterService.getAllUsers(input.actor),
             "Internal server error"
         );
     }
 
     public async getAllAdminUsers(request: Request): Promise<resp<User[] | undefined>> {
-        return this.withSuperAdmin(request, "getAllAdminUsers", "Error getting all admin users", (actor) =>
-            superAdminRequestAdapterService.getAllAdminUsers(actor),
+        return this.withSuperAdminInput(request, "getAllAdminUsers", "Error getting all admin users", (input) =>
+            superAdminRequestAdapterService.getAllAdminUsers(input.actor),
             "Internal server error"
         );
     }
 
-    private async withSuperAdmin<T>(
+    private async withSuperAdminInput<T>(
         request: Request,
         operation: string,
         errorLogPrefix: string,
-        action: (actor: User) => Promise<resp<T | undefined>>,
+        action: (input: SuperAdminServiceInput) => Promise<resp<T | undefined>>,
         internalErrorMessage = "Internal Server Error"
     ): Promise<resp<T | undefined>> {
         try {
@@ -52,10 +51,17 @@ export class SuperAdminService extends Service {
                 return createResponse(error.code, error.message);
             }
 
-            return action(actor);
+            return action(this.toServiceInput(request, actor));
         } catch (error: any) {
             logger.error(`${errorLogPrefix}: ${error.message ?? error}`);
             return createResponse(500, error.message ? `${internalErrorMessage}: ${error.message}` : internalErrorMessage);
         }
+    }
+
+    private toServiceInput(request: Request, actor: User): SuperAdminServiceInput {
+        return {
+            actor,
+            body: request.body
+        };
     }
 }
